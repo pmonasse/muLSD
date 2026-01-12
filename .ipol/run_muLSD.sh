ae2a118817ffc8e27fe73754774d3786430888cd
Pascal Monasse <pascal.monasse@enpc.fr>
2026-01-12 17:12:18 +0100

    PPM not supported by browsers -> BMP

--- .ipol/run_muLSD.sh
+++ .ipol/run_muLSD.sh
@@ -7,4 +7,4 @@ scales=$1
 
 $bin/build/muLSD -s $scales $input_0 mulsd.txt &&
 $bin/build2/draw_lines mulsd.txt lines.png &&
-(! [ -x composite ] || composite lines.png $input_0 linesOver.ppm)
+(! [ -x composite ] || composite lines.png $input_0 linesOver.bmp)
