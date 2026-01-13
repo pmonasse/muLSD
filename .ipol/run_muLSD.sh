35176dbabab732e4935cc14b8fa327c801217dc3
Pascal Monasse <pascal.monasse@enpc.fr>
2026-01-13 13:51:57 +0100

    BMP not supported by IPOL gallery -> PNG

--- .ipol/run_muLSD.sh
+++ .ipol/run_muLSD.sh
@@ -7,4 +7,4 @@ scales=$1
 
 $bin/build/muLSD -s $scales $input_0 mulsd.txt &&
 $bin/build2/draw_lines mulsd.txt lines.png &&
-(! [ -x composite ] || composite lines.png $input_0 linesOver.bmp)
+(! [ -x composite ] || composite lines.png $input_0 linesOver.png)
