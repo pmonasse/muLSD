9e418d8951bbbe8656cdce868beb9ea1cc1cf525
Pascal Monasse <pascal.monasse@enpc.fr>
2026-01-13 14:09:35 +0100

    Check with BMP and full path for composite (imagemagick)

--- .ipol/run_muLSD.sh
+++ .ipol/run_muLSD.sh
@@ -7,4 +7,5 @@ scales=$1
 
 $bin/build/muLSD -s $scales $input_0 mulsd.txt &&
 $bin/build2/draw_lines mulsd.txt lines.png &&
-(! [ -x composite ] || composite lines.png $input_0 linesOver.png)
+(! [ -x /usr/bin/composite ] ||
+        /usr/bin/composite lines.png $input_0 linesOver.bmp)
